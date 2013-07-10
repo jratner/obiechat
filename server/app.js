f7c32ca1b4a356b93d333c49333f9622fe9ff014
Jack Ratner <jackratner@gmail.com>
2013-07-10 15:18:49 -0700

    added some packages. removed require. beginning to play with orm and mysql.

--- server/app.js
+++ server/app.js
@@ -1,2 +1,21 @@
-// all requests will pass through socket.io (i think)
-// probably make our server here? I'll play with this tomorrow
+// the heart of the server
+var orm = require("orm");
+
+
+// load up the db!
+var opts = {
+    database: "obiechat",
+    protocol: "mysql",
+    host: "localhost",
+    user: "obiechatServer",
+    password: "obietalk" // teehee
+};
+
+orm.connect(opts, function(err, db) {
+    if (err) {
+	return new Error('Bad DB Connection');
+    }
+    
+    var Message = require("./models/message.js")(db);
+});
+
