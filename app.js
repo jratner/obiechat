50ae317a6616753296cd6284d8892da02ea44a13
Jack Ratner <jackratner@gmail.com>
2013-07-12 11:02:27 -0700

    some express config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
@@ -6,9 +7,16 @@ var path = require('path');
 var orm = require("orm");
 var port = process.env.PORT || 3000;
 
+app.configure(function(){
+    app.use('/media', express.static(__dirname + '/media'));
+    app.use(express.static(__dirname + '/public'));
+});
+
 server.listen(port, function() {
     console.log('listening on port', port);
 });
+
+app.use(express.logger());
 
 io.sockets.on('connection', function(socket) {
     console.log('we have a connection');
