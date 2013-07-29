6d3492916dd6bc7edf20aa8d8f5485c4f64f10a7
Jack Ratner <jackratner@gmail.com>
2013-07-28 17:14:50 -0700

    sessions with express and sockets

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ var fs = require('fs');
 var path = require('path');
 var orm = require("orm");
 var port = process.env.PORT || 3000;
+var occonf = require("./occonf.json");
 
 app.configure(function(){
     app.use('/media', express.static(__dirname + '/media'));
     app.use(express.static(__dirname + '/public'));
     app.use(express.static(__dirname + '/public/templates'));
+    
+    app.use(express.cookieParser());
+    var sessionPrefs = occonf.session;
+    app.use(express.session({secret: sessionPrefs.secret, key: sessionPrefs.key}));
+        
+    if (occonf.development) {
+        app.use(express.logger());
+    }
 });
 
 
@@ -18,7 +27,7 @@ server.listen(port, function() {
     console.log('listening on port', port);
 });
 
-app.use(express.logger());
+
 
 var db = require('./server/models/db.js');
 var sockets = require('./server/sockets/socketMain.js')(io, app);
--- server/sockets/socketMain.js
+++ server/sockets/socketMain.js
@@ -1,12 +1,30 @@
 module.exports = function(io, app) {
+    var cookieUtils = require('connect').utils;
+    var cookie = require('cookie');
     var messageSockets = require('./messageSockets.js')();
     var topicSockets = require('./topicSockets.js')();
     var userSockets = require('./userSockets.js')(app);
+    var occonf = require("../../occonf.json");
+    var sessionPrefs = occonf.session;
+
+
+    io.set('authorization', function(data, accept) {
+        if (data.headers.cookie) {
+            data.cookie = cookie.parse(data.headers.cookie);
+            data.sessionId = cookieUtils.parseSignedCookie(data.cookie[sessionPrefs.key], sessionPrefs.secret);
+            if (data.cookie[sessionPrefs.key] == data.sessionId) {
+                accept("Invalid cookie", false);
+            }
+        } else {
+            accept("No cookie sent with socket connection", false);
+        }
+        
+        accept(null, true);
+    });
     
     io.sockets.on('connection', function(socket) {
-        console.log('we have a connection');
         socket.emit('init', {data: 'hello from obiechat!'});
-        
+
         messageSockets.startListening(socket);
         topicSockets.startListening(socket);
         userSockets.startListening(socket);
