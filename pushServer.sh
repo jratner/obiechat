dabce02033a40c0c6a4003f1fd1a871b8ed91a14
Jack Ratner <jackratner@gmail.com>
2013-08-11 14:07:01 -0700

    no scripts directory, don't want to redirect everything right now.

--- pushServer.sh
+++ pushServer.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
 
-npm install
-bower install
-cp -r ../public/bower_comonents/* ../public/set_bower_comonents/
 ./compileLess.sh
 git push ssh://5206a9fe500446f6e500002b@obiechat-version001.rhcloud.com/~/git/obiechat.git/ master
